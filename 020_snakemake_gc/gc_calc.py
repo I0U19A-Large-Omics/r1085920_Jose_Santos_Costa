f29a24cb0f321bc75b2af4141f061f1e0c7ac53b
joseSantosCosta <josebouzaserrano@gmail.com>
2026-03-10 15:48:28 +0100

    Added error handling to the py script

--- 020_snakemake_gc/gc_calc.py
+++ 020_snakemake_gc/gc_calc.py
@@ -5,17 +5,32 @@ output_file = sys.argv[2]
 seq = ""
 GC = 0
 seq_id = ""
-with open(input_file) as f, open(output_file,'w') as out:
-    lines = [line.strip() for line in f.readlines()]
-    seq_id = lines[0][1:]
-    seq = "".join(lines[1:])
-    print(seq)
-    for n in seq:
-        if n == 'C' or n == 'G':
-            GC += 1
-    GC = str(GC/len(seq))
-    out.write('Sequence_id\tGC_percentage\n')
-    out.write(f'{seq_id}\t{GC}\n')
+valid_bases = {'A','T','C','G'}
+try:
+    with open(input_file) as f, open(output_file,'w') as out:
+        lines = [line.strip() for line in f.readlines()]
+        if lines == []:
+            raise ValueError("The file is empty")
+        if not lines[0].startswith(">"):
+            raise ValueError("File does not start with a FASTA header")
+        seq_id = lines[0][1:]
+        seq = "".join(lines[1:])
+        for base in seq:
+            if base not in valid_bases:
+                raise ValueError(f"Invalid base found:{base}")
+        for n in seq:
+            if n == 'C' or n == 'G':
+                GC += 1
+        try:
+            GC = str(GC/len(seq))
+        except ZeroDivisionError:
+            print("The sequence is empty so its not possible to find the GC%")
+        out.write('Sequence_id\tGC_percentage\n')
+        out.write(f'{seq_id}\t{GC}\n')
+except FileNotFoundError:
+    print(FileNotFoundError)
+    print("We cannot load the files provided")
+
 
 
     
