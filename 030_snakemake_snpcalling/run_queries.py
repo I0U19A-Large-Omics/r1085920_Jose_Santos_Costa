3fc101be52153d27defaa6d01ca25d3886dad1aa
joseSantosCosta <josebouzaserrano@gmail.com>
2026-04-29 20:35:42 +0200

    bug fixing

--- 030_snakemake_snpcalling/run_queries.py
+++ 030_snakemake_snpcalling/run_queries.py
@@ -3,8 +3,8 @@ import sqlite3
 db = sys.argv[1]
 con = sqlite3.connect(db)
 cur = con.cursor()
-print('Total SNPs:', cur.execute('SELECT COUNT(*) FROM SNP').fetchone()[0])
-print('Orphaned Effects:', cur.execute('SELECT COUNT(*) FROM Effect WHERE snp_id NOT IN (SELECT id FROM SNP)').fetchone()[0])
-for row in cur.execute('SELECT genotype, COUNT(*) FROM Call GROUP BY genotype').fetchall():
+print('Total SNPs:', cur.execute('SELECT COUNT(*) FROM SNPS').fetchone()[0])
+print('Orphaned Effects:', cur.execute('SELECT COUNT(*) FROM EFFECTS WHERE id NOT IN (SELECT id FROM SNPS)').fetchone()[0])
+for row in cur.execute('SELECT genotype, COUNT(*) FROM CALLS GROUP BY genotype').fetchall():
     print('Genotype:', row[0], 'Count:', row[1])
 con.close()
